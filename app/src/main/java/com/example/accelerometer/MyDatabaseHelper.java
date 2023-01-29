fedd880c139e6267e677ef1b4f5ea47b48df7c48
PatrikEdelenji <patrik.edelenji@gmail.com>
2023-01-29 16:20:55 +0100

    added some texts for explanation

--- app/src/main/java/com/example/accelerometer/MyDatabaseHelper.java
+++ app/src/main/java/com/example/accelerometer/MyDatabaseHelper.java
@@ -42,6 +42,20 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     }
 
+    /*
+    onUpgrade() is called (you do not call it yourself) when version of your DB changed which means underlying table structure changed etc.
+
+    In general it means that OS is telling you "hey, you asked for database structure version 10 but
+    I found we got something older here, so this is you chance to fix that before you start using database (and potentially crash due to structure mismatch)".
+
+    In that method you should do all that is necessary to, well.. upgrade structure of your old database to structure matching current version requirements
+    like adding/dropping columns, converting row contents or even dropping old db completely and create it from scratch
+    For Android it does not matter what you do here - it's just a sort of emergency callback for your code to do the necessary job (if any).
+    You need to be aware that users may not update frequently so you have to always handle upgrade from version X to Y knowing that X may not be equal to i.e. (Y-1).
+    https://stackoverflow.com/questions/13159210/onupgrade-sqlite-database-in-android
+
+    TL;DR IT GETS CALLED WHENEVER YOU CHANGE DB VERSION
+    */
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
