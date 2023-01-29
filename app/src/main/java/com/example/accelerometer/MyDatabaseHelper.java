f42cf6ea2c50cc9bdc6fa712e882e087e8457193
PatrikEdelenji <patrik.edelenji@gmail.com>
2023-01-29 16:08:42 +0100

    added onCreate(db)

--- app/src/main/java/com/example/accelerometer/MyDatabaseHelper.java
+++ app/src/main/java/com/example/accelerometer/MyDatabaseHelper.java
@@ -3,13 +3,16 @@ package com.example.accelerometer;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.util.Calendar;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private Context context;
-    private static final String DATABASE_NAME = "AccelerometerData.db";
+    //private static final String DATABASE_NAME = "AccelerometerData.db";
     private static final int DATABASE_VERSION = 1;
 
     private static final String TABLE_NAME = "MyAccelerometerData";
@@ -19,7 +22,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_ZVALUE = "zValue";
 
     public MyDatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        //super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, String.valueOf(Calendar.getInstance().getTime())+".db", null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
         this.context = context;
     }
 
@@ -28,15 +33,21 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
         String query = "CREATE TABLE " + TABLE_NAME +
                         " (" +
                         COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+
                         COLUMN_XVALUE + " REAL, " +
                         COLUMN_YVALUE + " REAL, " +
                         COLUMN_ZVALUE + " REAL)";
         db.execSQL(query);
+        Log.d("TAG database :", "DATABASE CREATED");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
 
     }
 }
+
+
